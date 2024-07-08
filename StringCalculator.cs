bcd0807946eca6a8842b1282af8ac1353326ae3a
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-08 20:23:02 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-public  class StringCalculator
+public class StringCalculator
 {
     public  int Add(string numbers)
     {
