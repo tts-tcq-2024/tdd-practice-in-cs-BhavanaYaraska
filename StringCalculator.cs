98d7fb61c9a597d5b3e8de2b8c2c9297f4658e85
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-07-08 20:03:46 +0530

    Update StringCalculator.cs

--- StringCalculator.cs
+++ StringCalculator.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-
 public static class StringCalculator
 {
     public static int Add(string numbers)
@@ -10,20 +5,38 @@ public static class StringCalculator
         if (string.IsNullOrEmpty(numbers))
             return 0;
 
-        // Default delimiters are comma and newline
-        string[] delimiters = { ",", "\n" };
+        string[] delimiters = GetDelimiters(numbers, out numbers);
 
-        // Check if custom delimiter is specified
+        string[] numberStrings = SplitNumbers(numbers, delimiters);
+
+        List<int> parsedNumbers = ParseNumbers(numberStrings);
+
+        return parsedNumbers.Sum();
+    }
+
+    private static string[] GetDelimiters(string numbers, out string trimmedNumbers)
+    {
+        string[] defaultDelimiters = { ",", "\n" };
+        trimmedNumbers = numbers;
+
         if (numbers.StartsWith("//"))
         {
             int delimiterIndex = numbers.IndexOf('\n');
             string customDelimiter = numbers.Substring(2, delimiterIndex - 2);
-            numbers = numbers.Substring(delimiterIndex + 1);
-            delimiters = new string[] { customDelimiter };
+            trimmedNumbers = numbers.Substring(delimiterIndex + 1);
+            return new string[] { customDelimiter };
         }
 
-        // Split numbers string using delimiters and convert to integers
-        string[] numberStrings = Regex.Split(numbers, string.Join("|", delimiters));
+        return defaultDelimiters;
+    }
+
+    private static string[] SplitNumbers(string numbers, string[] delimiters)
+    {
+        return Regex.Split(numbers, string.Join("|", delimiters));
+    }
+
+    private static List<int> ParseNumbers(string[] numberStrings)
+    {
         List<int> parsedNumbers = new List<int>();
 
         foreach (string numStr in numberStrings)
@@ -31,14 +44,23 @@ public static class StringCalculator
             if (!string.IsNullOrEmpty(numStr))
             {
                 int num = int.Parse(numStr);
-                if (num < 0)
-                    throw new ArgumentException($"negatives not allowed: {num}");
-
-                if (num <= 1000)
-                    parsedNumbers.Add(num);
+                ValidateNumber(num);
+                parsedNumbers.Add(num);
             }
         }
 
-        return parsedNumbers.Sum();
+        return parsedNumbers;
+    }
+
+    private static void ValidateNumber(int num)
+    {
+        if (num < 0)
+        {
+            throw new ArgumentException($"negatives not allowed: {num}");
+        }
+        else if (num > 1000)
+        {
+            // Numbers greater than 1000 are ignored
+        }
     }
 }
